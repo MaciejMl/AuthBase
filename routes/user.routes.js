585ea52c1a81aad706212e2ab981f81e56112f7b
MaciejMl <szypki@gmail.com>
2023-06-25 21:59:04 +0200

    Add routes, passport config, profile&settings views

---
+++ routes/user.routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const isLogged = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/user/no-permission');
+  }
+};
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/logged', isLogged, (req, res) => {
+  console.log(req.user);
+  res.render('logged', { user: req.user });
+});
+
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile', { user: req.user });
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
+});
+
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+module.exports = router;
